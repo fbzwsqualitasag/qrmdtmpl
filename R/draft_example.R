fef66a32222b7dd1d1fdaa58922c2325ae9d87c3
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2022-04-05 10:42:15 +0200

    Option for directly opening RMarkdown source file

--- R/draft_example.R
+++ R/draft_example.R
@@ -9,14 +9,15 @@
 #'
 #'
 #' @param ps_path target path where example report should be positioned
+#' @param pb_open should the example report source file be opened
 #'
 #' @export create_example_qprojektreport
 #'
 #' @examples
 #' tmprepdir <- tempdir()
-#' create_example_qprojektreport(ps_path = tmprepdir)
+#' create_example_qprojektreport(ps_path = tmprepdir, pb_open = FALSE)
 #' unlink(file.path(tmprepdir, "qprojekt_report"), recursive = TRUE, force = TRUE)
-create_example_qprojektreport <- function(ps_path){
+create_example_qprojektreport <- function(ps_path, pb_open = rlang::is_interactive()){
   # check whether ps_path exists
   if (!dir.exists(ps_path)) {
     cat("[create_example_qprojektreport] * Create target directory: ", ps_path, "\n")
@@ -27,6 +28,16 @@ create_example_qprojektreport <- function(ps_path){
   if (dir.exists(s_example_dir)) {
     cat("[create_example_qprojektreport] * Copy ", s_example_dir, " to: ", ps_path, "\n")
     fs::dir_copy(path = s_example_dir, new_path = ps_path)
+    # open report source file, if specified
+    if (pb_open){
+      s_bn_report <- basename(s_example_dir)
+      s_report_rmd <- file.path(ps_path, s_bn_report, paste(s_bn_report, ".Rmd", sep = ""))
+      if (file.exists(s_report_rmd)){
+        usethis::edit_file(s_report_rmd)
+      } else {
+        cat("[create_example_qprojektreport] * CANNOT FIND target report Rmd file: ", s_report_rmd, "\n")
+      }
+    }
   } else {
     cat("[create_example_qprojektreport] * CANNOT FIND example directory: ", s_example_dir, "\n")
   }
